abea048d7a1b4ce92fc3e6ef18b55e11409a10ba
Mayank Sandilya <78838306+beingkamikaze@users.noreply.github.com>
2023-09-14 18:33:08 +0530

    frontend

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,57 @@
+import { Flex, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
+import { useRecoilState } from "recoil";
+import postsAtom from "../atoms/postsAtom";
+
+const HomePage = () => {
+	const [posts, setPosts] = useRecoilState(postsAtom);
+	const [loading, setLoading] = useState(true);
+	const showToast = useShowToast();
+	useEffect(() => {
+		const getFeedPosts = async () => {
+			setLoading(true);
+			setPosts([]);
+			try {
+				const res = await fetch("/api/posts/feed");
+				const data = await res.json();
+				if (data.error) {
+					showToast("Error", data.error, "error");
+					return;
+				}
+				console.log(data);
+				setPosts(data);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setLoading(false);
+			}
+		};
+		getFeedPosts();
+	}, [showToast, setPosts]);
+
+	return (
+		<>
+			{!loading && posts.length === 0 && 
+			<div>
+				<h1>Follow some users to see the feed</h1>
+				Direction to follow users :-
+				<h2>Browse "http://localhost:3000/[USERNAME]"</h2>
+			</div>
+			}
+
+			{loading && (
+				<Flex justify='center'>
+					<Spinner size='xl' />
+				</Flex>
+			)}
+
+			{posts.map((post) => (
+				<Post key={post._id} post={post} postedBy={post.postedBy} />
+			))}
+		</>
+	);
+};
+
+export default HomePage;
